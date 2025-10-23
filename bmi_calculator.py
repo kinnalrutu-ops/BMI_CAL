902cd6b9fb4d2df5985f66fc72f7913d71b25aa1
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 15:16:28 +0530

    first commit

---
+++ bmi_calculator.py
@@ -0,0 +1,9 @@
+
+weight = float(input("Enter weight (in kg): "))
+height = float(input("Enter height (in meters): "))
+
+bmi = weight / (height ** 2)
+
+print("Weight:", weight, "kg")
+print("Height:", height, "m")
+print("BMI:", round(bmi, 1))
