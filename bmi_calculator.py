c6d9e37bcd58c23d006b3befdcd618a2356c957e
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 15:18:53 +0530

    save

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -7,3 +7,12 @@ bmi = weight / (height ** 2)
 print("Weight:", weight, "kg")
 print("Height:", height, "m")
 print("BMI:", round(bmi, 1))
+
+if bmi < 18.5:
+    category = "Underweight"
+elif 18.5 <= bmi < 25:
+    category = "Normal"
+else:
+    category = "Overweight"
+
+print("Category:", category)
